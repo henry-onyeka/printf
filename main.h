123d58e664ff5ade0c79bbe8ed1d9b43394f26fb
MICHAEL HENRY ONYEKA <hmike54@gmail.com>
2022-10-18 15:53:40 -0700

    Update main.h
    yt

--- main.h
+++ main.h
@@ -17,6 +17,19 @@ int _putchar(char c);
 int print_percent(va_list args);
 
 int (*get_func(char x))(va_list args);
+int print_rev(va_list va);
+int hexa_special(unsigned int c);
+int stringhexa(va_list va);
+int pointer(va_list va);
+int hexa_pointer(unsigned long int c);
+int binary(va_list va);
+int print_unsigned(va_list va);
+int hexa(va_list va);
+int hexa_upper(va_list va);
+int octal(va_list va);
+int print_rot(va_list va);
+char *malloc_str(char *str);
+char *rot13(char *p);
 
 typedef struct specifier
 {
