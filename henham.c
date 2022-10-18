be3cf225623a54cb4945e627af9a1ce44ac081ad
hmike54@gmail.com <hmike54@gmail.com>
2022-10-18 14:54:11 -0700

    all

--- henham.c
+++ henham.c
@@ -16,4 +16,26 @@ int print_c(va_list args)
 
 	return (_putchar(c));
 }
+/**
+ * print_s - prints a string
+ * @args: string argument
+ * Return: number of characters
+ */
+int print_s(va_list args)
+{
+	int i = 0, count = 0;
+	char *str;
+
+	i = 0;
+
+	str = va_arg(args, char*);
+	if (str == NULL)
+		str = "(null)";
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+		count++;
+	}
+	return (count);
 
