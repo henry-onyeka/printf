56cbfa0afcb1710f7f9096df1fbd1d494d5c6a32
MICHAEL HENRY ONYEKA <hmike54@gmail.com>
2022-10-18 17:03:08 -0700

    Update _printf.c
    here

--- _printf.c
+++ _printf.c
@@ -1,135 +1,52 @@
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-
+#include <stddef.h>
 /**
- * t_char - print a character
- *@va:character
- *
- * Return: no return
- */
-int t_char(va_list va)
-{
-	int c;
-
-	c = va_arg(va, int);
-	_putchar(c);
-	return (1);
-}
-/**
- * t_string - print a string
- *@va: pointer to string
- *
- * Return: no return
- */
-int t_string(va_list va)
-{
-	int i, j;
-	char n[] = "(null)";
-	char *s = va_arg(va, char *);
-
-	if (s == NULL)
-	{
-		for (i = 0; n[i] != '\0'; i++)
-			_putchar(n[i]);
-		return (6);
-	}
-	for (j = 0; s[j] != '\0'; j++)
-		_putchar(s[j]);
-	return (j);
-}
-/**
- * print_number - Entry point
- *@va: the integer to print
- * Return: no return
- */
-int print_number(va_list va)
-{
-	int i, len, r, l;
-	unsigned int abs, num, numt;
-	int n = va_arg(va, int);
-
-	len = 0;
-	i = 0;
-	r = 1;
-	l = 1;
-	if (n < 0)
-	{
-		_putchar('-');
-		len++;
-		abs = -n;
-	} else
-	{
-		abs = n;
-	}
-
-	num = abs;
-	while (num > 0)
-	{
-		num /= 10;
-		i++;
-	}
-
-	while (r < i)
-	{
-		l *= 10;
-		r++;
-	}
-	while (l >= 1)
-	{
-		numt = (abs / l) % 10;
-		_putchar(numt + '0');
-		len++;
-		l /= 10;
-	}
-	return (len);
-}
-
-/**
- * _printf - print output according to a format
- *@format: first argument
- *
- * Return: the number of characters printed(excluding the null byte)
+ * _printf - recreates the printf function
+ * @format: string with format specifier
+ * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	int i = 0, j, len = 0, count;
-	va_list valist;
-	types difftypes[] = {{'c', t_char}, {'s', t_string}, {'d', print_number},
-			     {'i', print_number}, {'b', binary}, {'u', print_unsigned},
-			     {'x', hexa}, {'X', hexa_upper}, {'o', octal}, {'R', print_rot},
-			     {'r', print_rev}, {'S', stringhexa}, {'p', pointer}};
-
-	if (format == NULL || (format[0] == '%' && format[1] == 0))
-		return (-1);
-	va_start(valist, format);
-	while (format != NULL && format[i])
+	if (format != NULL)
 	{
-		if (format[i] != '%')
-			len += _putchar(format[i]);
-		else
+		int count = 0, i;
+		int (*m)(va_list);
+		va_list args;
+
+		va_start(args, format);
+		i = 0;
+		if (format[0] == '%' && format[1] == '\0')
+			return (-1);
+		while (format != NULL && format[i] != '\0')
 		{
-			i++;
 			if (format[i] == '%')
-				len += _putchar('%');
-			j = 0;
-			count = 0;
-			while (j < 13)
 			{
-				if (format[i] == difftypes[j].t)
+				if (format[i + 1] == '%')
 				{
-					len += difftypes[j].f(valist);
-					count = 1;
-					break; }
-				j++; }
-			if (!count && format[i] != '%')
+					count += _putchar(format[i]);
+					i += 2;
+				}
+				else
+				{
+					m = get_func(format[i + 1]);
+					if (m)
+						count += m(args);
+					else
+						count = _putchar(format[i]) + _putchar(format[i + 1]);
+					i += 2;
+				}
+			}
+			else
 			{
-				len++;
-				len++;
-				_putchar('%');
-				_putchar(format[i]); }}
-		i++; }
-	va_end(valist);
-	return (len);
+				count += _putchar(format[i]);
+				i++;
+			}
+		}
+		va_end(args);
+		return (count);
+	}
+	return (-1);
 }
