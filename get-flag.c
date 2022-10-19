2f0819008946581135004aa535229185a79ea2fd
macisuman <110997734+macisuman@users.noreply.github.com>
2022-10-19 01:18:54 +0100

    Create get-flag.c
    flare

---
+++ get-flag.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * get_flag - func
+ * @s: param
+ * @f: param
+ * Return: int
+ */
+int get_flag(char s, flags_t *f)
+{
+	int i = 0;
+
+	switch (s)
+	{
+		case '+':
+			f->plus = 1;
+			i = 1;
+			break;
+		case ' ':
+			f->space = 1;
+			i = 1;
+			break;
+		case '#':
+			f->hash = 1;
+			i = 1;
+			break;
+	}
+
+	return (i);
+}
